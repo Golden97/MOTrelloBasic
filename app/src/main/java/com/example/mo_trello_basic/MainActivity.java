b67017e93feff9f112e8b3c7ced532a8f4b1d6cd
Bartosz <bklosekk@gmail.com>
2019-06-12 23:54:17 +0200

    Database - done

--- app/src/main/java/com/example/mo_trello_basic/MainActivity.java
+++ app/src/main/java/com/example/mo_trello_basic/MainActivity.java
@@ -1,27 +1,20 @@
 package com.example.mo_trello_basic;
 
-import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Room;
-import android.arch.persistence.room.RoomDatabase;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
-
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
+import static com.example.mo_trello_basic.TableActivity.taskListMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,6 +35,12 @@ public class MainActivity extends AppCompatActivity {
         items = new ArrayList<>();
         itemsAdapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, items);
+
+        List<TaskTable> list = db.taskTableDAO().getAllTaskTables()
+        for(TaskTable task : list) {
+            itemsAdapter.add(task.name);
+        }
+
         lvItems.setAdapter(itemsAdapter);
 
         lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
